016c72204725bb9487d231387e276d13719c7679
Alex-LiSun <96344734+Alex-LiSun@users.noreply.github.com>
2025-04-30 15:32:57 +0800

    fixed code

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:alex-lisun/Notes.git main:gh-pages
+git push -f git@github.com:Alex-LiSun/Notes.git main:gh-pages
 
 cd -
